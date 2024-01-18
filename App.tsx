d287ea01b4bffd1a416847ef45eacf31765570a2
liptonzuma <solomonboateng231@gmail.com>
2024-01-18 02:20:40 +0000

    done with tutorial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import Animated, {
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function App() {
+  const scale = useSharedValue(1);
+
+  useEffect(() => {
+    scale.value = withDelay(
+      1000,
+      withRepeat(
+        withSpring(2, {
+          duration: 2000,
+        }),
+        -1,
+        true
+      )
+    );
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[styles.circle, { transform: [{ scale }] }]} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
+  circle: {
+    width: 120,
+    height: 120,
+    borderRadius: 120,
+    backgroundColor: 'red',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
